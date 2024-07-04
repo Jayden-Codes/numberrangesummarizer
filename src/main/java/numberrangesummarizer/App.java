0d70ac48bccf955bd8d319ed474a49e5de05c7d2
Abrahams, J, Mr [23559322@sun.ac.za] <jabrahamsx@gmail.com>
2024-07-04 09:12:48 +0200

    Working on some test cases

---
+++ src/main/java/numberrangesummarizer/App.java
@@ -0,0 +1,67 @@
+package numberrangesummarizer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App implements NumberRangeSummarizer
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    /**
+     * @param input
+     * @return
+     */
+    @Override
+    public Collection<Integer> collect(String input) {
+        Collection<Integer> myCollection = new ArrayList<>();
+        String [] val = input.split(",");
+        for (String v: val) {
+            myCollection.add(Integer.parseInt(v));
+        }
+
+        return myCollection;
+    }
+
+    /**
+     * @param input
+     * @return
+     */
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        List<Integer> numbers = new ArrayList<>(input);
+        int numSize = numbers.size();
+        StringBuilder output = new StringBuilder();
+
+        for (int i= 0; i < numSize; i++) {
+            int start = numbers.get(i);
+            int end = start;
+            while (i + 1 < numSize && numbers.get(i+1) == numbers.get(i) + 1) {
+                end = numbers.get(i+1);
+                i++;
+            }
+            if (start == end) {
+                if(i + 1 < numSize) {
+                    output.append(start).append(", ");
+                } else {
+                    output.append(start);
+                }
+            } else {
+                if(i + 1 < numSize) {
+                    output.append(start).append("-").append(end).append(", ");
+                } else {
+                    output.append(start).append("-").append(end);
+                }
+            }
+        }
+
+        return output.toString();
+    }
+}
